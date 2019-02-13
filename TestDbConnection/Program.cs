49d68baff42997b4e1ed76269a7317ead9e9cf53
Mohammed <tuniis@live.com>
2019-02-12 22:23:16 -0600

    Test project to simulate connection leak

--- TestDbConnection/Program.cs
+++ TestDbConnection/Program.cs
@@ -1,12 +1,63 @@
-﻿using System;
-
-namespace TestDbConnection
+﻿namespace TestDbConnection
 {
-    class Program
+    using System;
+    using System.Data.SqlClient;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main.
+        /// Article:
+        /// https://github.com/aspnet/EntityFrameworkCore/issues/10169
+        /// https://github.com/dotnet/corefx/issues/24966
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+               select	count(*) as sessions,
+                        s.host_name,
+                        s.host_process_id,
+                        s.program_name,
+                        db_name(s.database_id) as database_name
+                from	sys.dm_exec_sessions s
+                where	is_user_process = 1
+                group by host_name, host_process_id, program_name, database_id
+                order by count(*) desc;             
+             */
+
+            var connectionString =
+                "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TestWebApi;Integrated Security=True;Connect Timeout=10;Max Pool Size=100;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Application Name=TestDbConnectionApp";
+
+            try
+            {
+                for (var i = 0; i < 500; i++)
+                {
+                    var sqlConnection = new SqlConnection(connectionString);
+                    
+                    using (var sqlCommand = new SqlCommand("SELECT * FROM Products", sqlConnection))
+                    {
+                        // Do not close connection to simulate connection leak
+                        sqlConnection.Open();
+                        Console.WriteLine($"Connection Open: {i}");
+
+                        var sqlDataReader = sqlCommand.ExecuteReader();
+                        sqlDataReader.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine("\n\nPress any key to exit.");
+            Console.Read();
         }
     }
 }
